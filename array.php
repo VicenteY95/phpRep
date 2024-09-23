1c3a0646a693526e83d2e11ff7d61fa3e99ab918
VicenteY95 <e3012786@student.dcccd.edu>
2024-09-23 09:09:30 -0500

    final

--- array.php
+++ array.php
@@ -6,11 +6,11 @@
     </head>
 <body>
 <?php
-echo $pageContent;
+echo $heading;
 $albumRating = ["Mm..Food" => "9", "Operation: Doomsday" => "8", "Vaudeville Villain" => "10", "Born like this" => "9", "Madvillainy" => "10"];
 $albumRating["Take me to your leader"] = "7";
 ksort($albumRating);
-$pageContent = "<h2>Favorite Albums</h2>";
+$heading = "<h2>Favorite Albums</h2>";
 foreach($albumRating as $name => $rating) {
     echo "Album= ". $name . ", Rating= " . $rating;
     echo "<br>"; 
@@ -19,29 +19,16 @@ foreach($albumRating as $name => $rating) {
 // multidimensional array
 
 $multi_array =array(
-    "The Beatles"=>array(
-        "Help!" => "1965",
-        "Rubber Soul"=> "1965",
-        "Abbey Road"=>"1969"
-    ),
-    "LedZepplin"=>array(
-        "Led Zepplin IV" => "1971"
-    ),
-    "Rolling Stones"=>array(
-        "Let It Bleed" => "1969",
-        "Sticky Fingers"=> "1971"
-    ),
-    "The Who"=>array(
-        "Tommy" => "1969",
-        "Quadrophenia"=> "1973",
-        "The Who by Numbers"=> "1973"
-    )        
+    "The Beatles"=>array("Help!" => "1965", "Rubber Soul"=> "1965", "Abbey Road"=>"1969"),
+    "LedZepplin"=>array("Led Zepplin IV" => "1971"),
+    "Rolling Stones"=>array("Let It Bleed" => "1969","Sticky Fingers"=> "1971"),
+    "The Who"=>array("Tommy" => "1969","Quadrophenia"=> "1973","The Who by Numbers"=> "1973")        
     );
     
-// • Write out the release date for Tommy by The Who.
+//  Access release date value for Tommy by The Who.
 echo $multi_array["The Who"]["Tommy"];
 
-// • Loop through the array and write out each artist and album title.
+// Loop through the array and access each artist and album title.
 foreach($multi_array as $x => $x_value) {
   echo "Artist=" . $x;
   echo "<br>";
@@ -53,6 +40,7 @@ foreach($multi_array as $x => $x_value) {
 }    
 
 // • Loop through the array and write out the each album and release date for The Who.
+echo "Release date for Tommy by the Who: ";
 echo "Artist=";
 echo "The Who"."<br>";
 echo "Albums= ";
