3016d9129c6d93be2b952c8244fbba894f93b523
VicenteY95 <e3012786@student.dcccd.edu>
2024-09-23 07:33:39 -0500

    passthru

--- array.php
+++ array.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
 <?php
-$albumRating = ["Mm..Food" => "9", "Operation: Doomsday" => "8", "Vaudeville Villain" => "10", "Born like this" => "9", "Madvillainy" => "10"] 
+$albumRating = ["Mm..Food" => "9", "Operation: Doomsday" => "8", "Vaudeville Villain" => "10", "Born like this" => "9", "Madvillainy" => "10"];
+$albumRating["Take me to your leader"] = "7";
+ksort($albumRating);
+$pageContent = "<h2>Favorite Albums</h2>";
+foreach($albumRating as $name => $rating) {
+    echo "Album". $name . ", Rating=" . $rating;
+    echo "<br>"; 
+}
 ?>
+</body>
+</html>
